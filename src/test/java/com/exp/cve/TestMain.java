5ffdbc8871de830c267b524bf5ff06a513acf508
EXP <exp.lqb@foxmail.com>
2020-09-09 18:21:35 +0800

    init

---
+++ src/test/java/com/exp/cve/TestMain.java
@@ -0,0 +1,13 @@
+package com.exp.cve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestMain {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
